0badf438ad08d59ee70db61d26db3c2a96d98e3f
Cuong Nguyen <ctring23@gmail.com>
2019-12-02 23:59:36 -0500

    Add zmq and protobuf

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,8 @@
 # stop on error
 set -e
 
-# sudo apt install libgtest-dev
+# Install toolings to compile dependencies
+sudo apt install autoconf automake libtool curl make g++ unzip libreadline-dev libgtest-dev pkg-config || true
 
 INSTALL_PREFIX=$PWD/.dep
 DOWNLOAD_DIR=$INSTALL_PREFIX/download
@@ -11,22 +12,22 @@ mkdir -p $INSTALL_PREFIX
 cd $INSTALL_PREFIX
 
 if [ -n "$(find $INSTALL_PREFIX -name 'libzmq.a')" ]; then
-  echo "Found libzmq. Skipping installation."
+  echo "Found zmq. Skipping installation."
 else
   mkdir -p $DOWNLOAD_DIR
   cd $DOWNLOAD_DIR
 
-  echo "Downloading libzmq"
-  wget https://github.com/zeromq/libzmq/releases/download/v4.3.2/zeromq-4.3.2.tar.gz 
+  echo "Downloading zmq"
+  wget -nc https://github.com/zeromq/libzmq/releases/download/v4.3.2/zeromq-4.3.2.tar.gz 
   tar -xzf zeromq-4.3.2.tar.gz
   rm -f zeromq-4.3.2.tar.gz
 
-  echo "Installing libzmq"
+  echo "Installing zmq"
   cd zeromq-4.3.2
   mkdir -p build
   cd build
   cmake .. -D WITH_PERF_TOOL=OFF -D ZMQ_BUILD_TESTS=OFF -D CMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX
-  make -j install
+  make -j$(nproc) install
   cd ../..
 
   cd ..
@@ -39,7 +40,7 @@ else
   mkdir -p $DOWNLOAD_DIR
   cd $DOWNLOAD_DIR
   echo "Downloading cppzmq"
-  wget https://github.com/zeromq/cppzmq/archive/v4.5.0.tar.gz
+  wget -nc https://github.com/zeromq/cppzmq/archive/v4.5.0.tar.gz
   tar -xzf v4.5.0.tar.gz
   rm -rf v4.5.0.tar.gz
 
@@ -48,9 +49,30 @@ else
   mkdir -p build
   cd build
   cmake .. -D CMAKE_INSTALL_PREFIX:PATH=$INSTALL_PREFIX
-  make -j install
+  make -j$(nproc) install
   cd ../..
 
   cd ..
   rm -rf $DOWNLOAD_DIR
 fi
+
+if [ -n "$(find $INSTALL_PREFIX -name 'libprotobuf*')" ]; then
+  echo "Found protobuf. Skipping installation."
+else
+  mkdir -p $DOWNLOAD_DIR
+  cd $DOWNLOAD_DIR
+  echo "Downloading protobuf"
+  wget -nc https://github.com/protocolbuffers/protobuf/releases/download/v3.11.1/protobuf-cpp-3.11.1.tar.gz
+  tar -xzf protobuf-cpp-3.11.1.tar.gz
+  rm -rf protobuf-cpp-3.11.1.tar.gz
+
+  echo "Installing protobuf"
+  cd protobuf-3.11.1
+  ./autogen.sh
+  ./configure --prefix=$INSTALL_PREFIX
+  make -j$(nproc) install
+  cd ..
+
+  cd ..
+  rm -rf $DOWNLOAD_DIR
+fi 
--- machine/server.cpp
+++ machine/server.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <google/protobuf/text_format.h>
+#include <proto/config.pb.h>
+#include <fstream>
+#include <sstream>
+
+using namespace slog;
 
 int main() {
-  std::cout << "Hello world!" << std::endl;
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  
+  std::ifstream f("slog.conf");
+  std::stringstream ss;
+  ss << f.rdbuf();
+  Configuration config;
+  google::protobuf::TextFormat::ParseFromString(ss.str(), &config);
+  std::string str;
+  google::protobuf::TextFormat::PrintToString(config, &str);
+  printf("%s", str.c_str());
+  return 0;
 }
