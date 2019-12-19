b931f38be1a4bee2547ad2a6c650ada6ee3653f8
Cuong Nguyen <ctring23@gmail.com>
2019-12-19 15:32:17 -0500

    Added Channel and ChannelListener

--- connection/channel.h
+++ connection/channel.h
@@ -8,16 +8,36 @@ namespace slog {
 
 class ChannelListener;
 
+/**
+ * A Channel is the interface between a module (e.g. Server, Sequencer) and the
+ * network layer. The Broker use the channels to pass message from the outside
+ * into the internal modules. To get the messages, a module would obtain the
+ * listener corresponding to a channel and poll messages via the listener.
+ */
 class Channel {
 public:
   Channel(std::shared_ptr<zmq::context_t> context, const std::string& name);
   Channel(const Channel&) = delete;
   Channel& operator=(const Channel&) = delete;
 
+  /**
+   * Returns a zmq pollitem data structure for this channel. This method is
+   * used by the Broker
+   */
   zmq::pollitem_t GetPollItem();
 
+  /**
+   * Passes a message to this channel. That message would be received by the
+   * listener
+   */
   void PassToListener(const MMessage& msg);
 
+  /**
+   * Returns a pointer to the listener corresponding to this channel.
+   * This methods should be called on a different thread other than the 
+   * Broker thread. Whoever owns this pointer later needs to free it up 
+   * otherwise the socket inside the listener would prevent a thread to exit.
+   */
   ChannelListener* GetListener();
 
 private:
@@ -26,15 +46,27 @@ private:
   zmq::socket_t socket_;
 };
 
+/**
+ * A ChannelListener corresponds to a Channel and is used by a module to get and send
+ * message from and to a channel
+ */
 class ChannelListener {
 public:
   ChannelListener(std::shared_ptr<zmq::context_t> context, const std::string& name);
 
   /**
-   * Returns false if timed out.
+   * Blocks until a request message arrived at the channel.
+   * Arguments:
+   *  msg         - To be filled with the next message in the channel
+   *  timeout_us  - If no message is received by the channel in after timeout_us 
+   *                microseconds, the method returns with false.
+   * Returns true if a message is received, false if timed out.
    */
   bool PollRequest(MMessage& msg, long timeout_us = -1);
 
+  /**
+   * Sends a response message to this channel.
+   */
   void SendResponse(const MMessage& msg);
 
 private:
