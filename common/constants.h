caa90923596ea6be811d27e2d8a903da4023a9fb
Cuong Nguyen <ctring23@gmail.com>
2020-01-03 22:15:28 -0500

    Reduce timeout

--- common/constants.h
+++ common/constants.h
@@ -4,9 +4,9 @@
 
 namespace slog {
 
-const long BROKER_POLL_TIMEOUT_MS = 100;
-const long SERVER_POLL_TIMEOUT_MS = 100;
-const long BASIC_MODULE_POLL_TIMEOUT_MS = 100;
+const long BROKER_POLL_TIMEOUT_MS = 50;
+const long SERVER_POLL_TIMEOUT_MS = 50;
+const long BASIC_MODULE_POLL_TIMEOUT_MS = 50;
 
 const std::string SERVER_CHANNEL("server");
 const std::string FORWARDER_CHANNEL("forwarder");
