6bfa0f94b0d4a2a3446e4a5e4377f208e434afd8
Cuong Nguyen <ctring23@gmail.com>
2020-02-10 17:13:11 -0500

    Parallelize data generation

--- tools/gen_data.py
+++ tools/gen_data.py
@@ -4,6 +4,7 @@ from __future__ import (
 )
 
 import base64
+import logging
 import numpy as np
 import os
 import string
@@ -12,10 +13,13 @@ if sys.version_info[0] == 3:
     _get_byte = lambda c: c
 else:
     _get_byte = ord
+import time
 
 from argparse import ArgumentParser
 from collections import defaultdict
+from functools import partial
 from google.protobuf.internal.encoder import _VarintBytes
+from multiprocessing import Pool
 from proto.offline_data_pb2 import Datum
 
 ALPHABET = np.array(list(string.ascii_lowercase + string.digits))
@@ -28,7 +32,14 @@ MULTIPLIERS = {
 
 KEY_SIZE = 12   # 8 bytes + overhead from base64 encoding
 MASTER_SIZE = 2
+FILE_EXTENSION = '.dat'
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(process)d - %(levelname)s: %(message)s'
+)
+LOG = logging.getLogger("gen_data")
+LOG_EVERY_SEC = 1
 
 def encode_key(key: int) -> bytes:
     """
@@ -56,12 +67,17 @@ class DataGenerator:
 
     def __init__(
         self,
+        data_dir: str,
+        prefix: str,
         num_replicas: int,
         num_partitions: int,
         size: int,
         size_unit: str,
         record_size: int,
+        max_jobs: int,
     ):
+        self.data_dir = os.path.abspath(data_dir)
+        self.prefix = prefix
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
@@ -69,11 +85,97 @@ class DataGenerator:
         num_records = total_size / (KEY_SIZE + record_size + MASTER_SIZE)
         self.num_records = int(num_records)
         self.record_size = record_size
+        self.max_jobs = max_jobs
 
     def key_to_partition(self, key: int) -> int:
         return fnv_hash(encode_key(key)) % self.num_partitions
 
-    def gen_datum(self, key: int, as_text=False):
+    def gen_data(self, as_text) -> None:
+        start_time = time.time()
+
+        # Identify the keys of each partition
+        LOG.info(
+            "Creating %d partitions from %d keys...", 
+            self.num_partitions, self.num_records
+        )
+        key_per_part = defaultdict(list)
+        for key in range(0, self.num_records):
+            p = self.key_to_partition(key)
+            key_per_part[p].append(key)
+
+        # Generate the actual data
+        num_jobs = (
+            min(self.num_partitions, self.max_jobs)
+            if self.max_jobs > 0 else self.num_partitions
+        )
+        LOG.info("Spawning %d jobs...", num_jobs)
+        func = partial(
+            DataGenerator.gen_data_per_partition,
+            as_text=as_text,
+            obj=self,
+        )
+        with Pool(num_jobs) as pool:
+            pool.starmap(func, key_per_part.items())
+
+        LOG.info("Done. Elapsed time: %.2f seconds", time.time() - start_time)
+    
+    @staticmethod
+    def gen_data_per_partition(
+        partition: int,
+        keys: list,
+        as_text: bool,
+        obj: object,
+    ):
+        '''
+        Wrapper for the class method __gen_data_per_partition so that it can be
+        used in multiprocessing.
+        '''
+        obj.__gen_data_per_partition(partition, keys, as_text)
+
+    def __gen_data_per_partition(
+        self,
+        partition: int,
+        keys: list,
+        as_text: bool,
+    ) -> None:
+        file_name = os.path.join(
+            self.data_dir,
+            self.prefix + str(partition) + FILE_EXTENSION
+        )
+        LOG.info("Generating data for %s", file_name)
+        mode = 'w' if as_text else 'wb'
+        part_file = open(file_name, mode)
+
+        # Write number of keys in this partition
+        if as_text:
+            part_file.write(str(len(keys)) + "\n")
+        else:
+            part_file.write(_VarintBytes(len(keys)))
+
+        last_time = time.time()
+        last_index = 0
+        for i, key in enumerate(keys):
+            # Generate the datum for this key
+            datum = self.__gen_datum(key, as_text)
+            # Append the datum to file
+            part_file.write(datum)
+
+            now = time.time()
+            if now - last_time >= LOG_EVERY_SEC:
+                pct = (i) / len(keys) * 100
+                rate = (i - last_index) / LOG_EVERY_SEC
+                LOG.info(
+                    "Progress: %d/%d (%.1f%%). Rate: %d datums/s",
+                    i + 1,
+                    len(keys),
+                    pct,
+                    rate)
+                last_time = now
+                last_index = i
+
+        part_file.close()
+
+    def __gen_datum(self, key: int, as_text=False):
         encoded_key = encode_key(key)
         record = ''.join(
             np.random.choice(
@@ -97,42 +199,6 @@ class DataGenerator:
             )
         
         return datum
-
-    def gen_data(self, data_dir: str, prefix='', as_text=False) -> None:
-        # Open a file for each partition
-        file_names = [
-            os.path.join(data_dir, prefix + str(p) + '.dat')
-            for p in range(self.num_partitions)
-        ]
-        mode = 'w' if as_text else 'wb'
-        part_files = [open(f, mode) for f in file_names]
-
-
-        # Count the number of keys for each partition and write to the
-        # beginning of its data file
-        key_counts = defaultdict(int)
-        for key in range(0, self.num_records):
-            p = self.key_to_partition(key)
-            key_counts[p] += 1
-
-        for p in key_counts:
-            c = key_counts[p]
-            if as_text:
-                part_files[p].write(str(c) + "\n")
-            else:
-                part_files[p].write(_VarintBytes(c))
-
-
-        # Generate the actual data
-        for key in range(0, self.num_records):
-            partition = self.key_to_partition(key)
-            datum = self.gen_datum(key, as_text)
-            part_files[partition].write(datum)
-
-
-        # Close the files
-        for p in part_files:
-            p.close()
 
 
 if __name__ == "__main__":
@@ -177,7 +243,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--as-text",
         action='store_true',
-        help="Generate data as text files"
+        help="Generate data as human-readable text files"
+    )
+    parser.add_argument(
+        "--max-jobs",
+        type=int,
+        default=0,
+        help="Maximum number of jobs spawned to do work. For unlimited number "
+             "of jobs, use 0."
     )
 
     args = parser.parse_args()
@@ -185,12 +258,14 @@ if __name__ == "__main__":
     np.random.seed(0)
 
     DataGenerator(
+        args.data_dir,
+        "",
         args.num_replicas,
         args.num_partitions,
         args.size,
         args.size_unit,
         args.record_size,
+        args.max_jobs,
     ).gen_data(
-        args.data_dir,
         as_text=args.as_text,
     )
