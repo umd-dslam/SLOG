2b8690da7c95ad2816b0af4f41c7992a018aaf59
Cuong Nguyen <ctring23@gmail.com>
2020-02-13 19:30:49 -0500

    Enqueue txns when we run out of workers

--- test/common/proto_utils_test.cpp
+++ test/common/proto_utils_test.cpp
@@ -14,16 +14,16 @@ TEST(ProtoUtilsTest, MachineIdStringToMachineId) {
   internal::MachineId mid;
 
   mid = MakeMachineId("0:0");
-  ASSERT_EQ(mid.replica(), 0);
-  ASSERT_EQ(mid.partition(), 0);
+  ASSERT_EQ(mid.replica(), 0U);
+  ASSERT_EQ(mid.partition(), 0U);
 
   mid = MakeMachineId("12:34");
-  ASSERT_EQ(mid.replica(), 12);
-  ASSERT_EQ(mid.partition(), 34);
+  ASSERT_EQ(mid.replica(), 12U);
+  ASSERT_EQ(mid.partition(), 34U);
 
   mid = MakeMachineId("5aa:6bb");
-  ASSERT_EQ(mid.replica(), 5);
-  ASSERT_EQ(mid.partition(), 6);
+  ASSERT_EQ(mid.replica(), 5U);
+  ASSERT_EQ(mid.partition(), 6U);
 
   ASSERT_THROW(MakeMachineId("01234"), std::invalid_argument);
   ASSERT_THROW(MakeMachineId("ab12:cd34"), std::invalid_argument);
