da6e29685574e1cee5d94e7e2b6b4cda2f84043c
Cuong Nguyen <ctring23@gmail.com>
2020-02-19 14:07:24 -0500

    Admin: Command to stream log from a server

--- tools/admin.py
+++ tools/admin.py
@@ -29,6 +29,7 @@ CONTAINER_DATA_DIR = "/var/tmp"
 HOST_DATA_DIR = "/var/tmp"
 
 SLOG_IMG = "ctring/slog"
+SLOG_CONTAINER_NAME = "slog"
 SLOG_DATA_MOUNT = docker.types.Mount(
     target=CONTAINER_DATA_DIR,
     source=HOST_DATA_DIR,
@@ -39,6 +40,22 @@ SLOG_CONFIG_FILE_PATH = os.path.join(
     CONTAINER_DATA_DIR,
     SLOG_CONFIG_FILE_NAME
 )
+
+
+def cleanup_container(
+    client: docker.DockerClient,
+    name: str,
+    addr="",
+) -> None:
+    """
+    Cleans up a container with a given name.
+    """
+    try:
+        c = client.containers.get(name)
+        c.remove(force=True)
+        LOG.info("%s: Cleaned up container \"%s\"", addr, name)
+    except:
+        pass
 
 
 class Command:
@@ -63,27 +80,32 @@ class Command:
 
     def create_subparser(self, subparsers):
         parser = subparsers.add_parser(self.NAME, help=self.HELP)
-        parser.add_argument("config", help="Path to a config file")
+        parser.add_argument(
+            "config",
+            metavar="config_file",
+            help="Path to a config file"
+        )
         parser.add_argument(
             "--local",
             action='store_true',
             help="Run the command on the local machine"
         )
-        parser.set_defaults(func=self.__initialize_and_do_command)
+        parser.set_defaults(run=self.__initialize_and_do_command)
         return parser
 
     def __initialize_and_do_command(self, args):
-        self.load_config_and_get_docker_clients(args)
+        self.load_config(args)
+        self.init_clients(args)
         self.pull_slog_image(args)
+
         self.do_command(args)
 
-    def load_config_and_get_docker_clients(self, args):
-        # Load SLOG config
+    def load_config(self, args):
         with open(args.config, "r") as f:
             self.config = Configuration()
             text_format.Parse(f.read(), self.config)
-        
-        # Initialize Docker clients
+    
+    def init_clients(self, args):
         self.rep_to_clients = []
         self.num_clients = 0
         for rep in self.config.replicas:
@@ -91,9 +113,7 @@ class Command:
             for addr in rep.addresses:
                 addr_str = addr.decode()
                 try:
-                    client = docker.DockerClient(
-                        base_url=f'ssh://{USER}@{addr_str}',
-                    )
+                    client = self.new_client(addr_str)
                     rep_clients.append((client, addr_str))
                     self.num_clients += 1
                     LOG.info("Connected to %s", addr_str)
@@ -126,30 +146,22 @@ class Command:
     ##############################
     #       Helper methods
     ##############################
+    def new_client(self, addr):
+        """
+        Gets a new Docker client for a given address.
+        """
+        return docker.DockerClient(
+            base_url=f'ssh://{USER}@{addr}',
+        )
+
     def clients(self) -> Tuple[docker.DockerClient, str]:
         """
-        Generator to iterate through all available docker clients
+        Generator to iterate through all available docker clients.
         """
         for clients in self.rep_to_clients:
             for (client, addr) in clients:
                 if client is not None:
                     yield (client, addr)
-    
-    def cleanup_container(
-        self,
-        client: docker.DockerClient,
-        addr: str,
-    ) -> None:
-        """
-        Cleans up a container whose name is the same as the name of the
-        corresponding command.
-        """
-        try:
-            c = client.containers.get(self.NAME)
-            c.remove(force=True)
-            LOG.info("%s: Cleaned up container \"%s\"", addr, self.NAME)
-        except:
-            pass
     
     def wait_for_containers(
         self,
@@ -194,7 +206,7 @@ class GenDataCommand(Command):
         )
         containers = []
         for client, addr in self.clients():
-            self.cleanup_container(client, addr)
+            cleanup_container(client, self.NAME, addr=addr)
             LOG.info(
                 "%s: Running command: %s",
                 addr,
@@ -242,10 +254,10 @@ class StartCommand(Command):
                     f"--partition {part} "
                     f"--data-dir {CONTAINER_DATA_DIR} "
                 )
-                self.cleanup_container(client, addr)
+                cleanup_container(client, SLOG_CONTAINER_NAME, addr=addr)
                 client.containers.run(
                     SLOG_IMG,
-                    name=self.NAME,
+                    name=SLOG_CONTAINER_NAME,
                     command=[
                         "/bin/sh", "-c",
                         sync_config_cmd + " && " +
@@ -277,7 +289,7 @@ class StopCommand(Command):
         for (client, addr) in self.clients():
             try:
                 LOG.info("Stopping SLOG on %s...", addr)
-                c = client.containers.get(StartCommand.NAME)
+                c = client.containers.get(SLOG_CONTAINER_NAME)
                 c.stop(timeout=0)
             except docker.errors.NotFound:
                 pass
@@ -303,7 +315,7 @@ class StatusCommand(Command):
                     status = "network unavailable"
                 else:
                     try:
-                        c = client.containers.get(StartCommand.NAME)
+                        c = client.containers.get(SLOG_CONTAINER_NAME)
                         status = c.status
                     except docker.errors.NotFound:
                         status = "container not started"
@@ -312,11 +324,86 @@ class StatusCommand(Command):
                 print(f"\tPartition {part} ({addr}): {status}")
 
 
+class LogsCommand(Command):
+
+    NAME = "logs"
+    HELP = "Stream logs from a server"
+
+    def create_subparser(self, subparsers):
+        parser = super().create_subparser(subparsers)
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument(
+            "-a",
+            metavar='ADDRESS',
+            help="Address of the machine to stream logs from"
+        )
+        group.add_argument(
+            "-rp",
+            nargs=2,
+            type=int,
+            metavar=('REPLICA', 'PARTITION'),
+            help="Two numbers representing the replica and"
+                 "partition of the machine to stream log from."
+        )
+        parser.add_argument(
+            "-f", "--follow", 
+            action="store_true",
+            help="Follow log output"
+        )
+
+    def init_clients(self, args):
+        """
+        Override this method because we only need one client
+        """
+        self.client = None
+        self.addr = None
+        try:
+            if args.a is not None:
+                addr_is_in_replica = (
+                    args.a.encode() in rep.addresses
+                    for rep in self.config.replicas
+                )
+                if all(addr_is_in_replica):
+                    self.addr = args.a
+                else:
+                    LOG.error(
+                        "Address \"%s\" is not specified in the config", args.a
+                    )
+                    return
+            else:
+                r, p = args.rp
+                self.addr = self.config.replicas[r].addresses[p].decode()
+
+            self.client = self.new_client(self.addr)
+            LOG.info("Connected to %s", self.addr)
+        except Exception as e:
+            LOG.error(str(e))
+
+    def do_command(self, args):
+        if self.client is None:
+            return
+
+        try:
+            c = self.client.containers.get(SLOG_CONTAINER_NAME)
+        except docker.errors.NotFound:
+            LOG.error("Cannot find container \"%s\"", SLOG_CONTAINER_NAME)
+            return
+
+        if args.follow:
+            try:
+                for log in c.logs(stream=True, follow=True):
+                    print(log.decode(), end="")
+            except KeyboardInterrupt:
+                print()
+        else:
+            print(c.logs().decode(), end="")
+
+
 if __name__ == "__main__":
     parser = ArgumentParser(
         description="Controls deployment and experiment of SLOG"
     )
-    subparsers = parser.add_subparsers(dest="command")
+    subparsers = parser.add_subparsers(dest="command name")
     subparsers.required = True
 
     COMMANDS = [
@@ -324,9 +411,10 @@ if __name__ == "__main__":
         StartCommand,
         StopCommand,
         StatusCommand,
+        LogsCommand,
     ]
     for command in COMMANDS:
         command().create_subparser(subparsers)
 
     args = parser.parse_args()
-    args.func(args)
+    args.run(args)
