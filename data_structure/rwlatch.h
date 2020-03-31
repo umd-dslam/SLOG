0ffa30887d9a8635c9bf1a9150ef24aa2fdd59e5
Cuong Nguyen <ctring23@gmail.com>
2020-03-31 19:42:08 -0400

    Added ConcurrentHashMap

---
+++ data_structure/rwlatch.h
@@ -0,0 +1,101 @@
+// Originally from CMU's BusTub project:
+// https://github.com/cmu-db/bustub/edit/master/src/include/common/rwlatch.h
+//
+// Slightly modified to work with this project.
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// rwmutex.h
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <climits>
+#include <condition_variable>  // NOLINT
+#include <mutex>               // NOLINT
+
+// Macros to disable copying and moving
+#define DISALLOW_COPY(cname)                             \
+  cname(const cname &) = delete;            /* NOLINT */ \
+  cname &operator=(const cname &) = delete; /* NOLINT */
+
+namespace bustub {
+
+/**
+ * Reader-Writer latch backed by std::mutex.
+ */
+class ReaderWriterLatch {
+  using mutex_t = std::mutex;
+  using cond_t = std::condition_variable;
+  static const uint32_t MAX_READERS = UINT_MAX;
+
+ public:
+  ReaderWriterLatch() = default;
+  ~ReaderWriterLatch() { std::lock_guard<mutex_t> guard(mutex_); }
+
+  DISALLOW_COPY(ReaderWriterLatch);
+
+  /**
+   * Acquire a write latch.
+   */
+  void WLock() {
+    std::unique_lock<mutex_t> latch(mutex_);
+    while (writer_entered_) {
+      reader_.wait(latch);
+    }
+    writer_entered_ = true;
+    while (reader_count_ > 0) {
+      writer_.wait(latch);
+    }
+  }
+
+  /**
+   * Release a write latch.
+   */
+  void WUnlock() {
+    std::lock_guard<mutex_t> guard(mutex_);
+    writer_entered_ = false;
+    reader_.notify_all();
+  }
+
+  /**
+   * Acquire a read latch.
+   */
+  void RLock() {
+    std::unique_lock<mutex_t> latch(mutex_);
+    while (writer_entered_ || reader_count_ == MAX_READERS) {
+      reader_.wait(latch);
+    }
+    reader_count_++;
+  }
+
+  /**
+   * Release a read latch.
+   */
+  void RUnlock() {
+    std::lock_guard<mutex_t> guard(mutex_);
+    reader_count_--;
+    if (writer_entered_) {
+      if (reader_count_ == 0) {
+        writer_.notify_one();
+      }
+    } else {
+      if (reader_count_ == MAX_READERS - 1) {
+        reader_.notify_one();
+      }
+    }
+  }
+
+ private:
+  mutex_t mutex_;
+  cond_t writer_;
+  cond_t reader_;
+  uint32_t reader_count_{0};
+  bool writer_entered_{false};
+};
+
+}  // namespace bustub
---
+++ test/data_structure/concurrent_hash_map_test.cpp
@@ -0,0 +1,162 @@
+#include <gtest/gtest.h>
+
+#include <thread>
+
+#include "data_structure/concurrent_hash_map.h"
+
+using namespace std;
+using namespace slog;
+
+TEST(ConcurrentHashMapTest, SerialBasicOperations) {
+  ConcurrentHashMap<string, string> map;
+  string result;
+  ASSERT_FALSE(map.Get(result, "test"));
+  ASSERT_FALSE(map.Erase("test"));
+
+  for (size_t i = 0; i < 10; i++) {
+    ASSERT_FALSE(map.InsertOrUpdate(to_string(i), "foo"));
+  }
+  for (size_t i = 0; i < 10; i++) {
+    ASSERT_TRUE(map.Get(result, to_string(i)));
+    ASSERT_EQ(result, "foo");
+  }
+  ASSERT_TRUE(map.InsertOrUpdate("0", "bar"));
+  ASSERT_TRUE(map.Get(result, "0"));
+  ASSERT_EQ(result, "bar");
+
+  for (size_t i = 0; i < 5; i++) {
+    ASSERT_TRUE(map.Erase(to_string(i)));
+  }
+  for (size_t i = 0; i < 5; i++) {
+    ASSERT_FALSE(map.Get(result, to_string(i)));
+  }
+  for (size_t i = 5; i < 10; i++) {
+    ASSERT_TRUE(map.Get(result, to_string(i)));
+    ASSERT_EQ(result, "foo");
+  }
+}
+
+TEST(ConcurrentHashMapTest, TriggerRehash) {
+  ConcurrentHashMap<string, string> map;
+  string result;
+
+  for (size_t i = 0; i < 10000; i++) {
+    ASSERT_FALSE(map.InsertOrUpdate(to_string(i), "foo" + to_string(i)));
+  }
+
+  for (size_t i = 0; i < 10000; i++) {
+    ASSERT_TRUE(map.Get(result, to_string(i))) << "Failed at i = " << i;
+    ASSERT_EQ(result, "foo" + to_string(i)) << "Failed at i = " << i;
+  }
+
+  for (size_t i = 0; i < 10000; i++) {
+    ASSERT_TRUE(map.Erase(to_string(i)));
+  }
+
+  for (size_t i = 0; i < 10000; i++) {
+    ASSERT_FALSE(map.Get(result, to_string(i)));
+  }
+}
+
+TEST(ConcurrentHashMapTest, TwoReadersOneWriter) {
+  uint32_t N = 500000;
+  string key = "foo";
+  ConcurrentHashMap<string, string> map;
+  
+  auto Updates = [&]() {
+    for (size_t i = 0; i < N; i++) {
+      map.InsertOrUpdate(key, to_string(i));
+    }
+  };
+
+  auto Gets = [&]() {
+    int prev = 0;
+    string result;
+    for (size_t i = 0; i < N; i++) {
+      if (map.Get(result, key)) {
+        auto x = stoi(result);
+        ASSERT_GE(x, prev);
+        prev = x;
+      }
+    }
+  };
+
+  thread w(Updates);
+  thread r1(Gets);
+  thread r2(Gets);
+  w.join();
+  r1.join();
+  r2.join();
+}
+
+TEST(ConcurrentHashMapTest, TwoWritersDifferentKeys) {
+  int N = 500000;
+  ConcurrentHashMap<string, string> map;
+  
+  auto Updates = [&](int start) {
+    for (int i = start; i < N; i += 2) {
+      map.InsertOrUpdate(to_string(i), to_string(i));
+    }
+  };
+
+  thread w1(Updates, 0);
+  thread w2(Updates, 1);
+  w1.join();
+  w2.join();
+
+  string result;
+  for (int i = 0; i < N; i++) {
+    ASSERT_TRUE(map.Get(result, to_string(i)));
+    ASSERT_EQ(result, to_string(i));
+  }
+}
+
+TEST(ConcurrentHashMapTest, TwoWritersSameKey) {
+  int N = 500000;
+  string key = "foo";
+  ConcurrentHashMap<string, string> map;
+  
+  auto Updates = [&]() {
+    for (int i = 0; i < N; i += 1) {
+      map.InsertOrUpdate(key, to_string(i));
+    }
+  };
+
+  thread w1(Updates);
+  thread w2(Updates);
+  w1.join();
+  w2.join();
+
+  string result;
+  ASSERT_TRUE(map.Get(result, key));
+  ASSERT_EQ(result, to_string(N-1));
+}
+
+TEST(ConcurrentHashMapTest, OneWriterOneEraser) {
+  int N = 100000;
+  ConcurrentHashMap<string, string> map;
+  
+  auto Updates = [&]() {
+    for (int i = 0; i < N; i++) {
+      map.InsertOrUpdate(to_string(i), to_string(i));
+    }
+  };
+
+  auto Erases = [&]() {
+    for (int i = 0; i < N; i++) {
+      map.Erase(to_string(i));
+    }
+  };
+
+  thread w1(Updates);
+  thread w2(Erases);
+  w1.join();
+  w2.join();
+
+  string result;
+  for (int i = 0; i < N; i++) {
+    if (map.Get(result, to_string(i))) {
+      ASSERT_EQ(result, to_string(i));
+    }
+  }
+}
