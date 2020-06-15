26280c8638c5c28165317745e0372f73bae3bf8c
ctring <ctring23@gmail.com>
2020-06-15 19:23:05 -0400

    Refactor Worker to support wait for confirmation

--- module/scheduler_components/worker.h
+++ module/scheduler_components/worker.h
@@ -21,12 +21,25 @@ using std::unordered_set;
 namespace slog {
 
 struct TransactionState {
+  enum class Phase {
+    READ_LOCAL_STORAGE,
+    WAIT_REMOTE_READ,
+    EXECUTE,
+    WAIT_CONFIRMATION,
+    COMMIT,
+    FINISH,
+  };
+
   TransactionState() = default;
   TransactionState(TransactionHolder* txn_holder) : txn_holder(txn_holder) {}
-  TransactionHolder* txn_holder;
-  uint32_t remote_reads_waiting_on;
+  TransactionHolder* txn_holder = nullptr;
+  uint32_t remote_messages_waiting_on = 0;
+  Phase phase = Phase::READ_LOCAL_STORAGE;
 };
 
+/**
+ * 
+ */
 class Worker : public Module {
 public:
   Worker(
@@ -38,15 +51,18 @@ public:
   void Loop() final;
 
 private:
-  void ProcessWorkerRequest(const internal::WorkerRequest& req);
-  TransactionState& InitializeTransactionState(TransactionHolder* txn);
-  void PopulateDataFromLocalStorage(Transaction* txn);
+  TxnId ProcessWorkerRequest(const internal::WorkerRequest& req);
+  TxnId ProcessRemoteReadResult(const internal::RemoteReadResult& read_result);
+  void AdvanceTransaction(TxnId txn_id);
 
-  void ProcessRemoteReadResult(const internal::RemoteReadResult& read_result);
-  
-  void ExecuteAndMaybeCommitTransaction(TxnId txn_id);
-  void ExecuteAndMaybeCommitTransactionHelper(TxnId txn_id);
+  void ReadLocalStorage(TxnId txn_id);
+  void Execute(TxnId txn_id);
+  void Commit(TxnId txn_id);
+  void Finish(TxnId txn_id);
 
+  void SendToOtherPartitions(
+      internal::Request&& request,
+      const std::unordered_set<uint32_t>& partitions);
   void SendToScheduler(
       const google::protobuf::Message& req_or_res,
       string&& forward_to_machine = "");
