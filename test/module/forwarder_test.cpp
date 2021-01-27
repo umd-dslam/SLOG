b10ce11a2dfa2c9dbd3a93b3ee48a2b0ee4a9cdf
Cuong Nguyen <ctring23@gmail.com>
2021-01-27 16:50:28 -0500

    Fix test

--- test/module/forwarder_test.cpp
+++ test/module/forwarder_test.cpp
@@ -159,14 +159,10 @@ TEST_F(ForwarderTest, TransactionHasNewKeys) {
 TEST_F(ForwarderTest, ForwardMultiHome) {
   // This txn involves data mastered by two regions
   auto txn = MakeTransaction({{"A"}, {"C", KeyType::WRITE}});
-  auto leader = configs[0]->leader_partition_for_multi_home_ordering();
-  auto non_leader = (1 + leader) % configs[0]->num_partitions();
 
-  // In replica 0, send to the partition that is not in charge of ordering multi-home txns
-  test_slogs[non_leader]->SendTxn(txn);
+  test_slogs[1]->SendTxn(txn);
+  auto forwarded_txn = ReceiveOnOrdererChannel(1);
 
-  auto forwarded_txn = ReceiveOnOrdererChannel(leader);
-  // The txn should be forwarded to the multihome orderer module of the leader
   ASSERT_TRUE(forwarded_txn != nullptr);
   ASSERT_EQ(TransactionType::MULTI_HOME_OR_LOCK_ONLY, forwarded_txn->internal().type());
   ASSERT_EQ(-1, forwarded_txn->internal().home());
