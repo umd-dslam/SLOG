b5a063ed3ba5fb1c9449d04d7cd32b6a9cdc841b
Cuong Nguyen <ctring23@gmail.com>
2021-05-07 23:41:37 -0400

    format

--- test/module/interleaver_test.cpp
+++ test/module/interleaver_test.cpp
@@ -137,9 +137,7 @@ class InterleaverTest : public ::testing::Test {
     }
   }
 
-  void SendToInterleaver(int from, int to, const Envelope& req) {
-    senders_[from]->Send(req, to, kInterleaverChannel);
-  }
+  void SendToInterleaver(int from, int to, const Envelope& req) { senders_[from]->Send(req, to, kInterleaverChannel); }
   void SendToLocalQueue(int from, int to, const Envelope& req) {
     auto copied = std::make_unique<Envelope>(req);
     senders_[from]->Send(std::move(copied), to, kLocalLogChannel);
