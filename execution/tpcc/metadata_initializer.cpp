adb7d0fbaaac671b7bc3b6e105f6a6bede40b77a
Cuong Nguyen <ctring23@gmail.com>
2021-07-05 18:54:53 -0400

    Fix tpcc metadata initializer

--- execution/tpcc/metadata_initializer.cpp
+++ execution/tpcc/metadata_initializer.cpp
@@ -11,7 +11,7 @@ TPCCMetadataInitializer::TPCCMetadataInitializer(uint32_t num_replicas, uint32_t
 Metadata TPCCMetadataInitializer::Compute(const Key& key) {
   CHECK_GE(key.size(), 4) << "Invalid key";
   uint32_t warehouse_id = *reinterpret_cast<const uint32_t*>(key.data());
-  return Metadata((warehouse_id / num_partitions_) % num_replicas_);
+  return Metadata(((warehouse_id - 1) / num_partitions_) % num_replicas_);
 }
 
 }  // namespace tpcc
